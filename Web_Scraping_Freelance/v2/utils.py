08551ef364e3516c57bd050d445f7801ebc2ca61
Daniel Matar <87388382+DansMTR@users.noreply.github.com>
2026-09-16 22:53:17 +0300

    Refactor utils.py for JSON configuration handling

--- Web_Scraping_Freelance/v2/utils.py
+++ Web_Scraping_Freelance/v2/utils.py
@@ -27,8 +27,8 @@ def write_configs(data, file_path):
 if __name__ == "__main__":
     # Data to be written to the JSON file
     data = {
-        "email": "nadine.schlamm1983@proton.me",
-        "password": "bWdS4pg8Ngn6Sw",
+        "email": "#########",
+        "password": "#########",
         "search_segments": [
             "keywords=",
             "countries=DE",
